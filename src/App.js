e50c73315efec71b0b565629c780f073ace09f71
trena <20079502+tholst952@users.noreply.github.com>
2024-09-20 13:57:15 -0700

    wip, need to reconfigure components to share all props except children

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import "./index.css";
 
 export default function App() {
   return (
     <div>
-      <TextExpander>
+      <TextExpander
+        collapsedNumWords={12}
+        collapsedNumWords={20}
+        expandButtonText="Show text"
+        collapseButtonText="Collapse text"
+        buttonColor="#ff6622"
+      >
         Space travel is the ultimate adventure! Imagine soaring past the stars
         and exploring new worlds. It's the stuff of dreams and science fiction,
         but believe it or not, space travel is a real thing. Humans and robots
@@ -24,7 +31,13 @@ export default function App() {
         foot on the moon or when rovers were sent to roam around on Mars.
       </TextExpander>
 
-      <TextExpander expanded={true} className="box">
+      <TextExpander
+        className="box"
+        collapsedNumWords={20}
+        expandButtonText="Show text"
+        collapseButtonText="Collapse text"
+        buttonColor="#ff6622"
+      >
         Space missions have given us incredible insights into our universe and
         have inspired future generations to keep reaching for the stars. Space
         travel is a pretty cool thing to think about. Who knows what we'll
@@ -34,6 +47,32 @@ export default function App() {
   );
 }
 
-function TextExpander() {
-  return <div>TODO</div>;
+function TextExpanderItem() {
+  return;
+}
+
+function TextExpander({
+  children,
+  className,
+  collapsedNumWords,
+  expandButtonText,
+  collapseButtonText,
+  buttonColor,
+  expanded,
+}) {
+  const [toggleText, setToggleText] = useState(false);
+
+  function handleToggle() {
+    setToggleText((toggleText) => !toggleText);
+    expanded = !expanded;
+  }
+
+  return (
+    <div className={className} expanded={expanded}>
+      {children.split(" ").slice(0, collapsedNumWords).join(" ")}
+      <button style={{ color: buttonColor }}>
+        {expanded ? collapseButtonText : expandButtonText}
+      </button>
+    </div>
+  );
 }
